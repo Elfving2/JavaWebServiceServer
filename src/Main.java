997e79febf05be6855a6d1dec327ce244571766f
Sebastian <sebastian.elfving@hotmail.com>
2023-03-31 14:38:39 +0200

    fixed server loops

--- src/Main.java
+++ src/Main.java
@@ -1,86 +1,102 @@
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        final int port = 6969;
         try {
-            ServerSocket serverSocket = new ServerSocket(port);
+            ServerSocket serverSocket = new ServerSocket(6969);
             Socket socket;
-
-
             while (true) {
-
-                try{
+                try {
                     socket = serverSocket.accept();
-                    // create a new socket everytime a new client connects
-
                     BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-                    while(true) {
-                        //Read what client has sent
-                        String messageFromClient = br.readLine();
-                        System.out.println("Client: " + messageFromClient);
-
-
-//                        String html = "<html><head><title>Java HTTP Server</title></head><body>" +
-//                                fetchJsonFromFile() + "</body></html>";
-//
-//                        final String CRLF = "\n\r";
-//
-//                        String response = "HTTP/1.1 200 OK" + CRLF + "Content-Length: " +
-//                                html.getBytes().length + CRLF + CRLF + html + CRLF + CRLF;
-
-
-
-                        //send back message to client
-                        bw.write(fetchJsonFromFile(messageFromClient).toJSONString());
-
-
+                    //Read what client has sent
+                    String messageFromClient = br.readLine();
+                    if(messageFromClient.equals("{\"close\":\"close\"}")) {
+                        break;
+                    } else {
+                        //hämta klientens meddelande och skicka den till openUpData
+                        String returnData = openUpData(messageFromClient);
+                        bw.write(returnData);
                         bw.newLine();
                         bw.flush();
                     }
+
                 } catch (IOException e) {
-                    System.out.println(e);
+                   throw new RuntimeException(e);
                 }
             }
-        } catch(IOException e) {
-            System.out.println(e);
-        }
-
-    }
-    static JSONObject fetchJsonFromFile (String message) {
-        String filePath = "data.json";
-
-        //hämta data från JSON fil
-        JSONObject fetchData = null;
-        try {
-            fetchData = (JSONObject) new JSONParser().parse(new FileReader(filePath));
-        } catch (ParseException e) {
-            throw new RuntimeException(e);
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
 
-        //konvertera data till ett JSONObject
-        JSONObject person = (JSONObject) fetchData.get(message);
+    static String openUpData(String message) {
+        //Steg 1, Bygg upp JSON object basserat på inkommande String
+        try {
+            JSONParser parser = new JSONParser();
+            //converte string to jsonobject
+            JSONObject jsonOb = (JSONObject) parser.parse(message);
 
+            System.out.println("option " + " = " + jsonOb);
+            //Steg 2, Läs av URL och HTTP-metod för att veta vad klienten vill
+            String url = jsonOb.get("httpURL").toString();
+            String method = jsonOb.get("httpMethod").toString();
 
+            //Steg 2.5, Dela upp URL med .split() metod
+            String[] urls = url.split("/");
 
-        //hämta och srkia ut data
-        //String nameP1 = p1.get("name").toString(), nameP2 = p2.get("name").toString();
-        //int ageP1 = Integer.parseInt(p1.get("age").toString()), ageP2 = Integer.parseInt(p2.get("age").toString());
+            //Steg 3, Anvämnd en switchCase för att kolla vilken data som skall användas
+            if (urls[0].equals("users")) {
+                if (method.equals("get")) {
+                    //hämta data om personer
+                    JSONObject jsonReturn = (JSONObject) parser.parse(new FileReader("data.json"));
 
+                    //inkludera HTTP status code
+                    jsonReturn.put("httpStatusCode", 200);
 
-        return person;
+                    //retunera jsons tring
+                    return jsonReturn.toJSONString();
+
+                } else if (method.equals("post")) {
+                    try {
+                        FileReader fileReader = new FileReader("data.json");
+                        JSONObject users = (JSONObject) parser.parse(fileReader);
+
+                        //create and add new user to "users"
+                        JSONObject newUser = new JSONObject();
+                        newUser.put("name", jsonOb.get("name"));
+                        newUser.put("age", jsonOb.get("age"));
+                        newUser.put("favoriteColor", jsonOb.get("favoriteColor"));
+                        JSONArray usersArray = (JSONArray) users.get("users");
+
+                        //add user to file
+                        usersArray.add(newUser);
+                        users.put("users", usersArray);
+                        FileWriter fileWriter = new FileWriter("data.json");
+                        fileWriter.write(users.toJSONString());
+
+                        fileWriter.close();
+                        fileReader.close();
+                        JSONObject success = new JSONObject();
+                        success.put("success", "you have successfully added a new user!");
+                        return success.toString();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        } catch (ParseException | IOException e) {
+            throw new RuntimeException(e);
+        }
+        return message;
     }
 }
